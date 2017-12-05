020ab418ec43989da7d575a4f125579d236a1c1c
Matt Hodges <matthewjhodges94@gmail.com>
2017-12-05 19:39:39 +0000

    update firebase info

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,7 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyD-VOQXjzqrx4jxCX9p79cNIyQthdCLJy8",
-    authDomain: "music-web-app-47bca.firebaseapp.com",
-    databaseURL: "https://music-web-app-47bca.firebaseio.com",
-    projectId: "music-web-app-47bca",
-    messagingSenderId: "296130970811"
+    //enter firebase details here
   };
